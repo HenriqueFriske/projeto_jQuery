f8b187e625186bec0042f2e03818351706cb894e
HenriqueFriske <henriquekulbabafriske@gmail.com>
2024-01-25 21:17:53 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+    $('#cancelar').click(function(){
+        $('form').slideUp();
+    })
+    
+    $('#taskForm').submit(function(e){
+        e.preventDefault();
+
+        var task = $('#tarefa').val();
+
+        $('#tasklist').append('<li>'+ task + '</li>');
+
+        $('#tarefa').val('');
+    });
+
+    $('#tasklist').on('click', 'li', function(){
+        $(this).toggleClass('completed');
+    });
+
+
+
+})
